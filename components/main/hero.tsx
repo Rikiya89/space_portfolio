f0c7ccff5f1a838e8e5b88a42e72561a2e586974
Rikiya Okawa <83910358+Rikiya89@users.noreply.github.com>
2024-04-11 11:21:01 +0900

    frist_commit

---
+++ components/main/hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const hero = () => {
+    return (
+    <div className='relative flex flex-col h-full w-full'>
+        <video autoPlay muted loop className='rotate-180 absolute top[-340px] left-0 z-[1] w-full h--full object-cover'>
+
+        </video>
+    </div>
+    )
+}
