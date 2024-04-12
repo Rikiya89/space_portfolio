d181ce799968157a2221f5ddbce0a6121ca65c73
Rikiya Okawa <83910358+Rikiya89@users.noreply.github.com>
2024-04-12 19:01:55 +0900

    20240412

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -20,24 +20,24 @@ const HeroContent = () => {
                 className='Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]'>
                     <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5"/>
                     <h1 className='Welcome-text text-[13px]'>
-                        Fullstack Developer Portfolio
+                        Rikiya Okawa Portfolio
                     </h1> 
                 </motion.div>
                 <motion.div 
                 variants={slideInFromLeft(0.5)}
-                className='flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto'>
+                className='flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[650px] w-auto h-auto'>
                     <span>
                     Providing
                     <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500'> the best </span>
-                    project experience
+                    immersive experience
                     </span>
                 </motion.div>
                 <motion.p
                     variants={slideInFromLeft(0.8)}
                     className='text-lg text-gray-400 my-5 max-w-[600]'
                 >
-                    I am a Full Stack Software Engineer with experience in Website,
-                    Mobile, ans Software development. Check out my projects and skills.
+                    I'm a Creative Technologist with experience in Website and XR development. 
+                    Check out my projects and skills.
                 </motion.p>
                 <motion.a
                 variants={slideInFromLeft(1)}
