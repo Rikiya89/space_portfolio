747291b83b1187e26de1d99e332c9b05d8f13daa
Rikiya Okawa <83910358+Rikiya89@users.noreply.github.com>
2024-04-25 17:14:53 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Encryption from "@/components/main/Encryption";
-import Hero from "@/components/main/Hero";
+import Hero from '@/components/main/Hero';
 import Projects from "@/components/main/Projects";
 import Skills from "@/components/main/Skills";
 import Image from "next/image";
