90a1c0e3c9af82ae6858a5084c5de84b42eb5ff0
Rikiya89 <83910358+Rikiya89@users.noreply.github.com>
2024-04-28 23:26:38 +0900

    Update Projects.tsx

--- app/components/main/Projects.tsx
+++ app/components/main/Projects.tsx
@@ -12,7 +12,7 @@ const Projects = () => {
       </h1>
       <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
         <ProjectCard
-          src="/NextWebsite.png"
+          src="/iphone14_clonewebsite.webp"
           title="Modern Next.js Portfolio"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
         />
