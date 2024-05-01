5e80adde6382b481cfb7fd32f433097c6d43a81c
Rikiya Okawa <83910358+Rikiya89@users.noreply.github.com>
2024-05-01 17:50:28 +0900

    20240501

--- app/components/main/Projects.tsx
+++ app/components/main/Projects.tsx
@@ -1,31 +1,33 @@
+// Projects.tsx
 import React from "react";
 import ProjectCard from "../sub/ProjectCard";
 
 const Projects = () => {
   return (
-    <div
-      className="flex flex-col items-center justify-center py-20"
-      id="projects"
-    >
+    <div className="flex flex-col items-center justify-center py-20" id="projects">
       <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
         My Projects
       </h1>
       <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
         <ProjectCard
           src="/iphone14_clonewebsite.webp"
-          title="Modern Next.js Portfolio"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          title="iPhone 14 Clone 3D Website"
+          description="Explore my 3D React portfolio—a symphony of code and design. Every click is a journey, with GSAP animations bringing the experience to life. It’s where my developer story unfolds, one interaction at a time."
+          url="https://apple-iphone3d-website.netlify.app/"  // Example project URL
         />
         <ProjectCard
-          src="/CardImage.png"
-          title="Interactive Website Cards"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          src="/iphone14_clonewebsite.webp"
+          title="iPhone 14 Clone 3D Website"
+          description="Explore my 3D React portfolio—a symphony of code and design. Every click is a journey, with GSAP animations bringing the experience to life. It’s where my developer story unfolds, one interaction at a time."
+          url="https://apple-iphone3d-website.netlify.app/"  // Example project URL
         />
         <ProjectCard
-          src="/SpaceWebsite.png"
-          title="Space Themed Website"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          src="/iphone14_clonewebsite.webp"
+          title="iPhone 14 Clone 3D Website"
+          description="Explore my 3D React portfolio—a symphony of code and design. Every click is a journey, with GSAP animations bringing the experience to life. It’s where my developer story unfolds, one interaction at a time."
+          url="https://apple-iphone3d-website.netlify.app/"  // Example project URL
         />
+        {/* Add other ProjectCards with their respective URLs */}
       </div>
     </div>
   );
