a5cc89e2263134255497e2b29bae7892634f10e9
Rikiya Okawa <83910358+Rikiya89@users.noreply.github.com>
2024-05-15 18:56:23 +0900

    20240515

---
+++ app/components/guardians_en/main/AboutMe.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AboutMeContent from "../sub/GuardiansContent";
+
+const Hero = () => {
+  return (
+    <div className="relative flex flex-col h-full w-full" id="about-me">
+      <AboutMeContent />
+    </div>
+  );
+};
+
+export default Hero;
